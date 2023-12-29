24b614d7e5cf03c5150ce2bb53ffc832bd22be25
okcha4ever <minoukhiari4@gmail.com>
2023-12-29 22:59:27 +0100

    feat: Hero section in homepage

---
+++ src/components/v0/Hero.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+
+export function Hero() {
+  return (
+    <>
+      <section className="w-full pt-12 md:pt-24 lg:pt-32">
+        <div className="container space-y-10 xl:space-y-16">
+          <div className="grid gap-4 px-10 md:grid-cols-2 md:gap-16">
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+            >
+              <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                Amine Khiari, <br />
+                Full-Stack Developer
+              </h1>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              className="flex flex-col items-start space-y-4"
+            >
+              <Image
+                src="/selife.jpg"
+                width={200}
+                height={200}
+                alt="amine khiari"
+                className="rounded-sm"
+              />
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                With over 3 years in the industry of tech, I specialize in
+                building scalable and optimized web applications with modern
+                tools.
+              </p>
+              <div className="space-x-4">
+                <Link
+                  className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                  href="#"
+                >
+                  View Projects
+                </Link>
+                <Link
+                  className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+                  href="#"
+                >
+                  Contact Me
+                </Link>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+        <hr className="mt-5" />
+      </section>
+    </>
+  );
+}
